4df200bb9225831c82ec5f2f79ebc7c0b9f4ac8a
Jakob Engler <jengler2291@gmail.com>
2023-05-24 12:05:00 -0500

    cats and dogs buttons on home page now functional, and direct to respective pages

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  const handleKittyButton = () => {
+    // to cats page
+    history.push('cats');
+  }
+
+  const handleDogoButton = () => {
+    // to dogs page
+    history.push('dogs');
+  }
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      {/* <p>Your ID is: {user.id}</p> */}
+      
       <div>
       </div>
       <h3>
         <img src="images/displatcat1.jpg" width = "300" height = "300"/>
-        <button>Cats</button>
+        <button onClick={handleKittyButton} >Cats</button>
         <img src="images/displaydog1.jpg" width = "300" height = "300"/>
-        <button>Dogs</button>
+        <button onClick={handleDogoButton} >Dogs</button>
         </h3>
 
         <h1>Mission Statment
        </h1>
        <p><p>Statement goes here</p></p>
+       <LogOutButton className="btn" />
       
       
     </div>
