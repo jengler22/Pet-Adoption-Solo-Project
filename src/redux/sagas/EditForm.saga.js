7c1eb70829a985826caca059cce050eda5169563
Jakob Engler <jengler2291@gmail.com>
2023-05-29 12:06:33 -0500

    created editForm saga

---
+++ src/redux/sagas/EditForm.saga.js
@@ -0,0 +1,17 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* editForm(action) {
+    try {
+        yield axios.put(`/api/lost_found/`, action.payload);
+    } catch (error) {
+        console.log(`Error in PUT for editRecipe`, error);
+    }
+}
+
+
+function* editFormSaga() {
+    yield takeEvery('UPDATE_FORM', editForm)
+}
+
+export default editFormSaga;
