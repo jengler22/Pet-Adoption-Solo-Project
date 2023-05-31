4d9c172255809813f0e38277d75465029138cd25
Jakob Engler <jengler2291@gmail.com>
2023-05-31 18:33:26 -0500

    created dogsList reducer.js

---
+++ src/redux/reducers/dogs.reducer.js
@@ -0,0 +1,10 @@
+const dogList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_DOGS':
+            return action.payload;
+        default:
+            return state;
+    }
+  }
+
+  export default dogList;
