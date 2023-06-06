e85cfbc6da667d8e932f0240fb41b93f02bb0f73
Jakob Engler <jengler2291@gmail.com>
2023-06-05 19:25:57 -0500

    added data to cats page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Save Them all Minnesota</h2>
+        <h2 className="nav-title">Save Them All Minnesota</h2>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
